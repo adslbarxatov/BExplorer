f491beee26855eb0e4a5401a1f3bcc021a6ec723
Николай <adslbarxatov@mail.ru>
2016-10-29 10:38:02 +1100

    Last update: 29.10.16

--- AudioScripts.h
+++ AudioScripts.h
@@ -17,12 +17,12 @@ union SD_AudioScriptStructure
 	struct AS_Formatted
 		{
 		uchar AS_Unused1[4];
-		uint AS_AudioNumber;
+		uint AS_AudioNumber;	// Номер аудиоэффекта
 		// 0 - alarm, 2 - police cell open/close, 4 - garage gate open/close, 6 - 'No escape' snore, 11 - shooting range target,
 		// 36 - 'Alloy Wheels of Steel' bikes, 37 - 'Messing with the Man' bikes, 38 - 'Hog tied' bikes, 39 - unused bikes sound,
 		// 40-42 - Malibu club, 43-45 - Pole position, 46 - Church, 51 - Fountain
 		uchar AS_Unused2[2];
-		float AS_PositionX;
+		float AS_PositionX;		// Координаты эффекта
 		float AS_PositionY;
 		float AS_PositionZ;
 		uchar AS_Unused3[4];
